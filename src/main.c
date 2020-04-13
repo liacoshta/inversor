a6d732497d2cc4f1f64642e20cda40cd3ed32555
Lia <lia.coshta@gmail.com>
2020-04-13 19:48:40 -0300

    agora vai

--- src/main.c
+++ src/main.c
@@ -1,4 +1,4 @@
-/* Data de submissao:17/03/2020
+ /* Data de submissao:17/03/2020
  * Nome: Lia Costa	
  * RA:159834
  */
@@ -24,25 +24,25 @@ int n_pilha=0;
 	return p;
 	}
 int main() {
-
+	int i=0;
   char buffer[tam_buffer];
- fgets(buffer, tam_buffer, stdin);
+ fgets(buffer, tam_buffer, stdin);	
+  	char c;
+			do{
+			if((buffer[i]!=' ')&&(buffer[i]!='\n')){
+				push(buffer[i]);
+				}else{//se foi espaço, deve remover e imprimir os caracteres do topo de pilha e continuar depois
 
-  char c;
-
-	
-	for (int i=0;i<tam_buffer;i++){	
-	//||buffer[i]!='\n'
-		if(buffer[i]!=' '){
-		push(buffer[i]);
-		}else{
-		//se foi espaço, deve remover e imprimir os caracteres do topo de pilha e continuar depois
-		while(n_pilha!=0){
-		c= pop();
-	 	printf("%c", c);
-		}
-		}
-	
-  	}
+					while(n_pilha!=0){
+						c= pop();
+				 		printf("%c", c);
+						}
+				
+				c=buffer[i];
+				printf("%c", c);
+				
+				}
+				i++;
+		} while (i!=tam_buffer);
   return 0;
 }
